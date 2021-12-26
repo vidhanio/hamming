1c32ee6b24ce0633037af93792424d3b44b42abc
Vidhan Bhatt <me@vidhan.io>
2021-12-25 22:43:13 -0500

    change name to code

--- hamming.go
+++ hamming.go
@@ -5,44 +5,45 @@ import (
 	"math/rand"
 )
 
-type Grid [4][4]bool
+type Code [4][4]bool
 
-func New() *Grid {
-	var grid Grid
+func New() *Code {
+	code := new(Code)
+
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
-			grid[i][j] = rand.Intn(2) == 0
+			code[i][j] = rand.Intn(2) == 0
 		}
 	}
 
-	colOne := grid.getColumn(1)
-	colTwo := grid.getColumn(3)
+	colOne := code.getColumn(1)
+	colTwo := code.getColumn(3)
 
-	grid[0][1] = getParity(append(colOne[1:], colTwo[:]...))
+	code[0][1] = getParity(append(colOne[1:], colTwo[:]...))
 
-	colOne = grid.getColumn(2)
-	colTwo = grid.getColumn(3)
+	colOne = code.getColumn(2)
+	colTwo = code.getColumn(3)
 
-	grid[0][2] = getParity(append(colOne[1:], colTwo[:]...))
+	code[0][2] = getParity(append(colOne[1:], colTwo[:]...))
 
-	rowOne := grid.getRow(1)
-	rowTwo := grid.getRow(3)
+	rowOne := code.getRow(1)
+	rowTwo := code.getRow(3)
 
-	grid[1][0] = getParity(append(rowOne[1:], rowTwo[:]...))
+	code[1][0] = getParity(append(rowOne[1:], rowTwo[:]...))
 
-	rowOne = grid.getRow(2)
-	rowTwo = grid.getRow(3)
+	rowOne = code.getRow(2)
+	rowTwo = code.getRow(3)
 
-	grid[2][0] = getParity(append(rowOne[1:], rowTwo[:]...))
+	code[2][0] = getParity(append(rowOne[1:], rowTwo[:]...))
 
-	return &grid
+	return code
 }
 
-func (g *Grid) String() string {
+func (c *Code) String() string {
 	var s string
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
-			s += fmt.Sprintf("%v", g[i][j])
+			s += fmt.Sprintf("%v", c[i][j])
 		}
 		s += "\n"
 	}
@@ -61,47 +62,47 @@ func getParity(bits []bool) bool {
 	return trues%2 == 0
 }
 
-func (g *Grid) getColumn(col int) [4]bool {
+func (c *Code) getColumn(col int) [4]bool {
 	var column [4]bool
-	for r, row := range g {
+	for r, row := range c {
 		column[r] = row[col]
 	}
 	return column
 }
 
-func (g *Grid) getRow(row int) [4]bool {
-	return g[row]
+func (c *Code) getRow(row int) [4]bool {
+	return c[row]
 }
 
-func (g *Grid) checkOne() bool {
-	colOne := g.getColumn(1)
-	colTwo := g.getColumn(3)
+func (c *Code) checkOne() bool {
+	colOne := c.getColumn(1)
+	colTwo := c.getColumn(3)
 
 	return getParity(append(colOne[1:], colTwo[:]...)) == colOne[0]
 }
 
-func (g *Grid) checkTwo() bool {
-	colOne := g.getColumn(2)
-	colTwo := g.getColumn(3)
+func (c *Code) checkTwo() bool {
+	colOne := c.getColumn(2)
+	colTwo := c.getColumn(3)
 
 	return getParity(append(colOne[1:], colTwo[:]...)) == colOne[0]
 }
 
-func (g *Grid) checkThree() bool {
-	rowOne := g.getRow(1)
-	rowTwo := g.getRow(3)
+func (c *Code) checkThree() bool {
+	rowOne := c.getRow(1)
+	rowTwo := c.getRow(3)
 
 	return getParity(append(rowOne[1:], rowTwo[:]...)) == rowOne[0]
 }
 
-func (g *Grid) checkFour() bool {
-	rowOne := g.getRow(2)
-	rowTwo := g.getRow(3)
+func (c *Code) checkFour() bool {
+	rowOne := c.getRow(2)
+	rowTwo := c.getRow(3)
 
 	return getParity(append(rowOne[1:], rowTwo[:]...)) == rowOne[0]
 }
 
-func (g *Grid) FindCorruption() (x, y int) {
+func (c *Code) FindCorruption() (x, y int) {
 	possible := [4][4]bool{
 		{true, true, true, true},
 		{true, true, true, true},
@@ -109,10 +110,10 @@ func (g *Grid) FindCorruption() (x, y int) {
 		{true, true, true, true},
 	}
 
-	checkOne := g.checkOne()
-	checkTwo := g.checkTwo()
-	checkThree := g.checkThree()
-	checkFour := g.checkFour()
+	checkOne := c.checkOne()
+	checkTwo := c.checkTwo()
+	checkThree := c.checkThree()
+	checkFour := c.checkFour()
 
 	for i := 0; i < 4; i++ {
 		if checkOne {
@@ -151,28 +152,24 @@ func (g *Grid) FindCorruption() (x, y int) {
 	return 0, 0
 }
 
-func (g *Grid) IsCorrupt() bool {
-	x, y := g.FindCorruption()
+func (c *Code) IsCorrupt() bool {
+	x, y := c.FindCorruption()
 	return x != 0 && y != 0
 }
 
-func (g *Grid) FixCorruption() {
-	x, y := g.FindCorruption()
-	g[x][y] = !g[x][y]
+func (c *Code) FixCorruption() {
+	x, y := c.FindCorruption()
+	c[x][y] = !c[x][y]
 }
 
-func (g *Grid) Check() bool {
-	return g.checkOne() && g.checkTwo() && g.checkThree() && g.checkFour()
-}
-
-func (g *Grid) Corrupt() (x, y int) {
+func (c *Code) Corrupt() (x, y int) {
 	x = rand.Intn(4)
 	y = rand.Intn(4)
 	for x+y == 0 {
 		x = rand.Intn(4)
 		y = rand.Intn(4)
 	}
-	g[x][y] = !g[x][y]
+	c[x][y] = !c[x][y]
 
 	return
 }
